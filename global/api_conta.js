9e0ce48f125c1fd181cb0f2a7ab816311784419e
IagoKcruz <iagokniphoff.ncg@gmail.com>
2023-12-08 22:56:30 -0300

    07/12

---
+++ global/api_conta.js
@@ -0,0 +1,24 @@
+const my_headers ={
+    "Content-Type": "application/json"
+}
+
+export async function get_user(){
+    const res = await fetch("http://localhost:3001/users")
+    const resJson = await res.json();
+    console.log(resJson)
+}
+
+export async function login(email, password){
+    const user = {
+        email : email ,
+        password : password 
+    }
+    const bodyJson = JSON.stringify(user)
+    const res = await fetch(
+        "http://localhost:3001/login",
+    {
+        headers: my_headers,
+        method: "POST",
+        body:bodyJson
+    })
+}
